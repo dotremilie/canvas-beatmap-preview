adf82736da4fbd00288fe8cdaba2d294d7523c04
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-01-04 23:53:54 +0100

    Rename

--- src/previewers/StandardBeatmapPreviewer.ts
+++ src/previewers/StandardBeatmapPreviewer.ts
@@ -2,7 +2,7 @@ import BeatmapPreviewer from "./BeatmapPreviewer.ts";
 import {StandardBeatmap, StandardRuleset} from "osu-standard-stable";
 import StandardRenderer from "../renderers/StandardRenderer.ts";
 
-export default class StandardBeatmapPreview extends BeatmapPreviewer<StandardBeatmap, StandardRenderer> {
+export default class StandardBeatmapPreviewer extends BeatmapPreviewer<StandardBeatmap, StandardRenderer> {
     constructor(id: string) {
         super(id, new StandardRuleset(), (ctx, beatmap) => new StandardRenderer(ctx, beatmap));
     }
