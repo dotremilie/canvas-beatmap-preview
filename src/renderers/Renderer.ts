520fb9029e5905f7d74ca79a1b342fa97c6fe200
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-01-04 22:50:48 +0100

    Create a generic Renderer class

---
+++ src/renderers/Renderer.ts
@@ -0,0 +1,21 @@
+export default abstract class Renderer<TBeatmap, THitObject> {
+    protected ctx: CanvasRenderingContext2D;
+    protected beatmap: TBeatmap;
+    protected drawableHitObjects: THitObject[] = [];
+
+    constructor(ctx: CanvasRenderingContext2D, beatmap: TBeatmap) {
+        this.ctx = ctx;
+        this.beatmap = beatmap;
+        this.initializeDrawableHitObjects();
+    }
+
+    /**
+     * Initializes drawable hit objects for the specific renderer.
+     */
+    protected abstract initializeDrawableHitObjects(): void;
+
+    /**
+     * Renders the frame for the given time.
+     */
+    public abstract render(time: number): void;
+}
